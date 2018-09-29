5e98a5e4dc4329cf869a949d6dbd677adcc81abf
Jay Shah <s.jay93@gmail.com>
2018-09-29 15:13:33 -0500

    Modified-2

--- src/main/java/com/yash/userloginregister/Application.java
+++ src/main/java/com/yash/userloginregister/Application.java
@@ -2,10 +2,12 @@ package com.yash.userloginregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 /**
  * The type Application.
  */
+@EnableOAuth2Sso
 @SpringBootApplication
 public class Application {
 
